7839f592b9194a75afd19301b36bde13af2a2466
Amaleia Kolovos <Amme@MacBook-Air-5.domain>
2019-06-21 19:57:41 -0600

    cleaning up code in views file - alert levels, loop breaks, title case

--- full-code-files/TravelAdvisoryApp/views.py
+++ full-code-files/TravelAdvisoryApp/views.py
@@ -15,39 +15,45 @@ def advisory(request):
 
     # if the user has provided info (method == 'POST')
     else:
-        form = CountryForm(request.POST)
+        form_input = CountryForm(request.POST)
 
         # validating and cleaning data for use
-        if form.is_valid():
-            form = form.cleaned_data
+        if form_input.is_valid():
+            form = form_input.cleaned_data
+        else:
+            error_message = 'Please enter a country name.'
+            return render(request, 'Advisory/advisory.html', {'form': CountryForm(), 'error': error_message})
 
-        # grabbing user input for country
-        country = form['country']
-        # alter data to fit - all country names capitalized in url data
-        country = country.capitalize()
-
+        # grabbing user input for country, alter data to fit - all country names capitalized in url data
+        country = form['country'].title()
+        
         # url with information from state department on travel warnings
         url = 'https://travel.state.gov/_res/rss/TAsTWs.xml'
-
-        # getting data from url and then parsing it into a dictionary form (from xml)
+        # getting data from url
         r = requests.get(url)
-        data = xmltodict.parse(r.content)
+        
+        # in case travel.state.gov is down
+        if r.status_code != 200:
+            print(r.status_code)
+            error_message = 'The server is currently unavailable. Please try again later. If problem persists, contact administrator.'
+            return render(request, 'Advisory/advisory.html', {'form': CountryForm(), 'error': error_message})
+
+        # parsing data into dictionary form (from xml) and
         # grabbing information on the actual countries out of the dictionary for easier access below
+        data = xmltodict.parse(r.content)
         x = data['rss']['channel']['item']
 
         # scanning through the dictionary for the country chosen by the user and grabbing that country's data
         country_info = ''
-        i = 0
         for item in x:
-            if country in "'+{}+'".format(x[i]['title']):
-                country_info = x[i]
-            i += 1
+            if country in "'+{}+'".format(item['title']):
+                country_info = item
+                break
 
         # in case the country cannot be found
         if country_info == '':
-            error_message = 'No information on {} could be found at this time. Please check that the ' \
+            error_message = 'No information on \"{}\" could be found at this time. Please check that the ' \
                            'country name is spelled correctly and try again.'.format(country)
-            print(error_message)
             return render(request, 'Advisory/advisory.html', {'form': CountryForm(), 'error': error_message})
 
         title_full = country_info['title']
@@ -61,27 +67,23 @@ def advisory(request):
         description = description_long.p.text
 
         # scanning through the country data info (it's title) to grab the warning level. Levels range from 1-4
-        i = 1
-        while i < 5:
+        for i in range(1,5):
             if 'Level {}'.format(i) in country_info['title']:
                 alert_level = i
-            i += 1
+                break
         # assigning color based on alert level for in-line styling for background and font on the html section
-        if alert_level == 1:
-            alert_color = 'DarkBlue'
-            font_color = 'White'
-        elif alert_level == 2:
-            alert_color = 'Yellow'
-            font_color = 'Black'
-        elif alert_level == 3:
-            alert_color = 'Orange'
-            font_color = 'Black'
-        elif alert_level == 4:
-            alert_color = 'Red'
-            font_color = 'Black'
+        alert = {
+            1: {'color': 'DarkBlue', 'font': 'White'},
+            2: {'color': 'Yellow', 'font': 'Black'},
+            3: {'color': 'Orange', 'font': 'Black'},
+            4: {'color': 'Red', 'font': 'Black'}
+            }
+        alert_color = alert[alert_level]['color']
+        alert_font = alert[alert_level]['font']
 
+        # compiling data to be sent back to html
         warning_info = {'country': country, 'title': title, 'date': date, 'link': link, 'description': description,
-                        'alert_color': alert_color, 'font_color': font_color}
+                        'alert_color': alert_color, 'font_color': alert_font}
 
         # returning information to be displayed and blank form
         return render(request, 'Advisory/advisory.html', {'form': CountryForm(), 'info': warning_info})
